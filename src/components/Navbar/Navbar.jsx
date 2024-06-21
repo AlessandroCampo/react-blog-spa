2aaf359e487c57afcea80b7efe054823438d2b2c
Alessandro Campo <alessandrocampo97@gmail.com>
2024-06-21 15:29:58 +0200

    feat: react router setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/images/boolbook-favicon-color.png'
 import { IoHome, IoHomeOutline, IoSearchOutline, IoHeart, IoHeartOutline } from "react-icons/io5";
 import { RiUserLine, RiUserFill } from 'react-icons/ri';
 import { Avatar } from '@mui/material';
-
+import { NavLink } from 'react-router-dom';
 
 export default ({ user }) => {
     return (
@@ -11,18 +11,35 @@ export default ({ user }) => {
         <menu className='flex flex-col justify-between items-start h-screen py-8 ps-6'>
             <img src={logo} alt="logo" className='w-[58px]' />
             <div className="icons-container text-gray-400 flex flex-col gap-6 text-4xl items-center">
-                <IoHomeOutline
-                    className='navbar-icon'
-                />
-                <IoSearchOutline
-                    className='navbar-icon'
-                />
-                <IoHeartOutline
-                    className='navbar-icon'
-                />
-                <RiUserLine
-                    className='navbar-icon'
-                />
+                <NavLink
+                    to="/"
+                >
+                    <IoHomeOutline
+                        className='navbar-icon'
+                    />
+                </NavLink>
+                <NavLink>
+                    <IoSearchOutline
+                        className='navbar-icon'
+                    />
+                </NavLink>
+                <NavLink>
+                    <IoHeartOutline
+                        className='navbar-icon'
+                    />
+                </NavLink>
+                <NavLink
+                    to={`/${user?.username}`}
+                >
+                    <RiUserLine
+                        className='navbar-icon'
+
+                    />
+                </NavLink>
+
+
+
+
             </div>
             <div className="navbar-bottom text-white flex flex-col gap-2">
                 <Avatar
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,24 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './views/Home.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
